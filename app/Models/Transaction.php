c615504d22b3c648fca97f623db6a6d04ce81005
darektoa <darektoa@gmail.com>
2021-11-01 15:36:33 +0700

    Added: Many to many relationship between transactions and products table

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,4 +10,9 @@ class Transaction extends Model
     use HasFactory;
 
     protected $guarded  = ['id'];
+
+
+    public function products() {
+        return $this->belongsToMany(Product::class);
+    }
 }
---
+++ database/migrations/2021_11_01_083023_create_product_transaction.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTransaction extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_transaction', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_transaction');
+    }
+}
